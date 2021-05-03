9a31f9f73b893aacadb6887b4c4d0a1f9466ddd4
Magnús Sveinsson <magnus10@ru.is>
2021-05-03 22:35:43 +0000

    finished setting things up

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="product-index"),
-    path('<int:id>', views.get_product_by_id, name="product-details")
+    path('<int:id>', views.get_product_by_id, name="product_details"),
+    path('create_product', views.create_product, name='create_product'),
+    path('create_category', views.create_category, name='create_category'),
+    path('create_manufacturer', views.create_manufacturer, name='create_manufacturer'),
+    path('delete_product/<int:id>', views.delete_product, name='delete_product'),
+    path('update_product/<int:id>', views.update_product, name='update_product')
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+from user.forms.profile_form import ProfileForm
+from user.models import Profile
 
 
 def index(request):
     return render(request, 'user/index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    return render(request, 'user/register.html', {
+        'form': UserCreationForm()
+    })
+
+
+def logout_redirect(request):
+    return render(request, 'user/logout_redirect.html')
+
+
+def profile(request):
+    profile = Profile.objects.filter(user=request.user).first()
+    if request.method == 'POST':
+        form = ProfileForm(instance=profile, data=request.POST)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('profile')
+    return render(request, 'user/profile.html', {
+        'form': ProfileForm(instance=profile)
+    })
