1e52113df50841aad692cbb4784c8a9a548390f8
stefan-ingi <si.arnason@gmail.com>
2021-05-10 21:21:07 +0000

    updates, removed bad code

--- cart/models.py
+++ cart/models.py
@@ -2,28 +2,10 @@ from django.db import models
 from django.conf import settings
 from products.models import Product
 # Create your models here.
-
-class ProductCategory(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
-
-class ProductManufacturer(models.Model):
-    name = models.CharField(max_length=255)
-    logo = models.CharField(max_length=9999, blank=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Cart(models.Model):
     name = models.CharField(max_length=255)
-    description = models.CharField(max_length=9999, blank=True)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
-    price = models.FloatField()
-    on_sale = models.BooleanField()
+    quantity = models.FloatField()
     manufacturer = models.ForeignKey(ProductManufacturer, on_delete=models.CASCADE)
 
     def __str__(self):
