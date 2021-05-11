b64082576e46da2918b7f4c246f05e0bbfd6f760
stefan-ingi <si.arnason@gmail.com>
2021-05-11 21:30:19 +0000

    adding buy function to products

--- cart/forms/cart_form.py
+++ cart/forms/cart_form.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm, widgets
 from django import forms
 
-from cart.models import CartAddress
+from cart.models import CartAddress, CartItems
 
 
 class CartAddressForm(ModelForm):
@@ -14,4 +14,15 @@ class CartAddressForm(ModelForm):
             'house_number': widgets.TextInput(attrs={'class': 'form-control'}),
             'city': widgets.TextInput(attrs={'class': 'form-control'}),
             'postal_code': widgets.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
+class CartUpdateForm(ModelForm):
+    class Meta:
+        model = CartItems
+        exclude = ['id', 'cart_id']
+        widgets = {
+            'product' : widgets.TextInput(attrs={'class': 'form-control'}),
+            'quantity' : widgets.TextInput(attrs={'class': 'form-control'}),
+            'total_price' : widgets.TextInput(attrs={'class': 'form-control'}),
         }
