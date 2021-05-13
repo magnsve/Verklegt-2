e1b90619c78a4c056cb555d34194b1f25241454e
lilja <liljas17@ru.is>
2021-05-13 11:33:14 +0000

    Forsíða

--- cart/models.py
+++ cart/models.py
@@ -1,37 +1,3 @@
 from django.db import models
-from products.models import Product
-from user.models import Profile
 
-
-class Cart(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    is_open = models.BooleanField(default=True)
-
-
-class CartItems(models.Model):
-    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.FloatField()
-    total_price = models.FloatField()
-
-
-class Country(models.Model):
-    country = models.CharField(max_length=255)
-
-
-class CartAddress(models.Model):
-    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=255)
-    street_name = models.CharField(max_length=255)
-    house_number = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    country_id = models.ForeignKey(Country, on_delete=models.CASCADE)
-    postal_code = models.CharField(max_length=255)
-
-
-class CartPayment(models.Model):
-    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    cardholder_name = models.CharField(max_length=255)
-    card_number = models.FloatField()
-    expiration_date = models.FloatField()
-    cvc = models.FloatField()
+# Create your models here.
--- cart/views.py
+++ cart/views.py
@@ -1,18 +1,5 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
-
-from cart.forms.cart_form import CartAddressForm
-from cart.models import Cart, CartAddress
+from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'cart/index.html')
-
-
-@login_required
-def contact(request):
-    profile_id = request.user.profile.id
-    cart = Cart.objects.filter(profile_id=profile_id).filter(is_open=True)
-    contact_info = CartAddress.objects.filter(cart_id=cart.id)
-    form = CartAddressForm(data=request.POST)
     return render(request, 'cart/index.html')
